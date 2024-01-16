6e6272ec71eb2c7bde465b568157e5c1f7a907ef
codeninjaug <segayiandrew@gmail.com>
2024-01-17 01:37:31 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,115 @@
+<script setup>
+import { reactive } from "vue";
+const form = reactive([
+  {
+    firstname: "",
+    lasttname: "",
+    password: "",
+  },
+]);
+
+const add = () => {
+  form.push({
+    firstname: "",
+    lasttname: "",
+    password: "",
+  });
+};
+
+const sub = (index) => {
+  if (index > 0) {
+    form.splice(index, 1);
+  }
+};
+</script>
+<template>
+  <div class="flex justify-center items-center gap-x-4 mt-10">
+    <svg
+      @click="add"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class="w-6 h-6 cursor-pointer"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+      />
+    </svg>
+  </div>
+  <div v-for="(item, index) in form" :key="index">
+    <div class="w-full mx-auto mt-20 max-w-lg">
+      <svg
+        @click="sub(index)"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-6 h-6 cursor-pointer"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+        />
+      </svg>
+      <div class="flex flex-wrap -mx-3 mb-6">
+        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+          <label
+            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            for="grid-first-name"
+          >
+            First Name
+          </label>
+          <input
+            v-model="item.firstname"
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            id="grid-first-name"
+            type="text"
+            placeholder="Jane"
+          />
+          <p class="text-red-500 text-xs italic">Please fill out this field.</p>
+        </div>
+        <div class="w-full md:w-1/2 px-3">
+          <label
+            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            for="grid-last-name"
+          >
+            Last Name
+          </label>
+          <input
+            v-model="item.lastname"
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="grid-last-name"
+            type="text"
+            placeholder="Doe"
+          />
+        </div>
+      </div>
+      <div class="flex flex-wrap -mx-3 mb-6">
+        <div class="w-full px-3">
+          <label
+            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            for="grid-password"
+          >
+            Password
+          </label>
+          <input
+            v-model="item.password"
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="grid-password"
+            type="password"
+            placeholder="******************"
+          />
+          <p class="text-gray-600 text-xs italic">
+            Make it as long and as crazy as you'd like
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
