7796080dae3773504551f849e3c593f0099b24a7
codeninjaug <segayiandrew@gmail.com>
2024-01-24 05:51:56 +0300

    -----ui changes

--- src/App.vue
+++ src/App.vue
@@ -82,7 +82,7 @@ const sub = (index) => {
             Last Name
           </label>
           <input
-            v-model="item.lastname"
+            v-model="item.lasttname"
             class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
             id="grid-last-name"
             type="text"
